627f64ebc2f2acd1d33fae282750ae6092ff2452
medivo <dakuh@volny.cz>
2017-05-06 19:39:41 +0200

    Initial commit

---
+++ Utility.py
@@ -0,0 +1,319 @@
+﻿"""File containing the utility classes"""
+from __future__ import absolute_import, division, print_function
+#Filesystem
+import os
+#Threading
+import threading
+#Colors
+from colorama import init, Fore, Back, Style
+init()
+#Sounds
+import winsound
+#Date
+import datetime as dt
+#Logging
+import json
+import logging
+import logging.config
+
+class DataStorage(object):
+    """Calss containing methods for CSV file manipulation"""
+    @staticmethod
+    def save_data(data, path, name, index_name='index', headers=True, b_append=False):
+        """ 
+        Saves passed data to CSV file 
+
+        Parameters 
+        ---------- 
+        data : dataframe 
+            Data to be saved 
+        path : string 
+            Where to save the data 
+        name : string 
+            Name of the file 
+        index_name : string
+            Header label for the index column 
+        """
+        #Check if folder exists 
+        if not os.path.exists(path):
+            os.makedirs(path)
+        elif b_append & os.path.exists(path + name + '.csv'):
+            #Append to existing file
+            data_org = pd.read_csv(path + name + '.csv', header=0, low_memory=False, index_col=0)
+            data = data_org.append(data)
+        data.to_csv(path + name + '.csv', index_label=index_name, header=headers)
+
+class Counter(object):
+    """Class for inter-thread counting"""
+    def __init__(self, start=0):
+        """
+        Init counter
+        
+        Parameters
+        ==========
+        @start : int
+            Initial value of the counter
+        """
+        self.lock = threading.Lock()
+        self.value = start
+    def increment(self):
+        """Increase value"""
+        self.lock.acquire()
+        try:
+            self.value = self.value + 1
+        finally:
+            self.lock.release()
+    def decrement(self):
+        """Decrease value"""
+        self.lock.acquire()
+        try:
+            self.value = self.value - 1
+        finally:
+            self.lock.release()
+
+class Logger(object):
+    def __init__(self, **kwargs):
+        self.setup_logging()
+    def setup_logging(self, default_path='./logs/logging.json', default_level=logging.INFO,
+                      env_key='LOG_CFG'):
+        """Setup logging configuration"""
+        path = default_path
+        value = os.getenv(env_key, None)
+        if value:
+            path = value
+        if os.path.exists(path):
+            with open(path, 'rt') as f:
+                config = json.load(f)
+            logging.config.dictConfig(config)
+        else:
+            logging.basicConfig(level=default_level)
+
+    """Helper class containing usefull utilities"""
+    def info(self, msg="", color="", level=0, file_logger_name='default_file_logger',
+             b_separator=False, b_sound=False, b_file=True):
+        """
+        Prints log message to console
+
+        Params
+        ==============
+        @msg : string
+            Message to be shown
+        @level : int
+            Indent level
+        @b_separator : Boolean
+            Show visual separator after the message?
+        @color : Fore.color
+            Color of the text
+        @b_sound : bool
+            Play sound after the message?
+        """
+        #Prepare loggers
+        #Fancy console
+        fancy_log = logging.getLogger('fancy_console_logger')
+        #File
+        file_log = logging.getLogger(file_logger_name)
+        #Default offset
+        offset = ''
+        #Print offset depending on the level
+        for i in range(0, level):
+            offset += '>>'
+        #Print message if there is one
+        if msg != "":
+            fancy_log.info(color  + offset + ' ' + msg + Style.RESET_ALL)
+            if b_file:
+                file_log.info(offset + ' ' + msg)
+        #Print separator if requested
+        if b_separator:
+            sep = ''
+            #Make the separator nicely aligned
+            for i in range(0, 25 - level):
+                sep += '=='
+            fancy_log.info(color + offset + ' ' + sep + Style.RESET_ALL)
+            if b_file:
+                file_log.info(offset + ' ' + sep)
+        #Play sound if requested
+        if b_sound:
+            winsound.Beep(1000,1000)
+    def error(self, msg="", color="", level=0, file_logger_name='default_file_logger',
+             b_separator=False, b_sound=False, b_file=True):
+        """
+        Prints log message to console
+
+        Params
+        ==============
+        @msg : string
+            Message to be shown
+        @level : int
+            Indent level
+        @b_separator : Boolean
+            Show visual separator after the message?
+        @color : Fore.color
+            Color of the text
+        @b_sound : bool
+            Play sound after the message?
+        """
+        #Prepare loggers
+        #Fancy console
+        fancy_log = logging.getLogger('fancy_console_logger')
+        #File
+        file_log = logging.getLogger(file_logger_name)
+        #Default offset
+        offset = ''
+        #Print offset depending on the level
+        for i in range(0, level):
+            offset += '>>'
+        #Print message if there is one
+        if msg != "":
+            fancy_log.error(color  + offset + ' ' + msg + Style.RESET_ALL)
+            if b_file:
+                file_log.error(offset + ' ' + msg)
+        #Print separator if requested
+        if b_separator:
+            sep = ''
+            #Make the separator nicely aligned
+            for i in range(0, 25 - level):
+                sep += '=='
+            fancy_log.error(color + offset + ' ' + sep + Style.RESET_ALL)
+            if b_file:
+                file_log.error(offset + ' ' + sep)
+        #Play sound if requested
+        if b_sound:
+            winsound.Beep(1000,1000)
+    def debug(self, msg="", color="", level=0, file_logger_name='default_file_logger',
+             b_separator=False, b_sound=False, b_file=True):
+        """
+        Prints log message to console
+
+        Params
+        ==============
+        @msg : string
+            Message to be shown
+        @level : int
+            Indent level
+        @b_separator : Boolean
+            Show visual separator after the message?
+        @color : Fore.color
+            Color of the text
+        @b_sound : bool
+            Play sound after the message?
+        """
+        #Prepare loggers
+        #Fancy console
+        fancy_log = logging.getLogger('fancy_console_logger')
+        #File
+        file_log = logging.getLogger(file_logger_name)
+        #Default offset
+        offset = ''
+        #Print offset depending on the level
+        for i in range(0, level):
+            offset += '>>'
+        #Print message if there is one
+        if msg != "":
+            fancy_log.debug(color  + offset + ' ' + msg + Style.RESET_ALL)
+            if b_file:
+                file_log.debug(offset + ' ' + msg)
+        #Print separator if requested
+        if b_separator:
+            sep = ''
+            #Make the separator nicely aligned
+            for i in range(0, 25 - level):
+                sep += '=='
+            fancy_log.debug(color + offset + ' ' + sep + Style.RESET_ALL)
+            if b_file:
+                file_log.debug(offset + ' ' + sep)
+        #Play sound if requested
+        if b_sound:
+            winsound.Beep(1000,1000)
+    def warning(self, msg="", color="", level=0, file_logger_name='default_file_logger',
+             b_separator=False, b_sound=False, b_file=True):
+        """
+        Prints log message to console
+
+        Params
+        ==============
+        @msg : string
+            Message to be shown
+        @level : int
+            Indent level
+        @b_separator : Boolean
+            Show visual separator after the message?
+        @color : Fore.color
+            Color of the text
+        @b_sound : bool
+            Play sound after the message?
+        """
+        #Prepare loggers
+        #Fancy console logger
+        #Supports colors
+        fancy_log = logging.getLogger('fancy_console_logger')
+        #File logger
+        file_log = logging.getLogger(file_logger_name)
+        #Default offset
+        offset = ''
+        #Print offset depending on the level
+        for i in range(0, level):
+            offset += '>>'
+        #Print message if there is one
+        if msg != "":
+            #Console log
+            fancy_log.warning(color  + offset + ' ' + msg + Style.RESET_ALL)
+            #Lof to file as well
+            if b_file:
+                file_log.warning(offset + ' ' + msg)
+        #Print separator if requested
+        if b_separator:
+            sep = ''
+            #Make the separator nicely aligned
+            for i in range(0, 25 - level):
+                sep += '=='
+            fancy_log.warning(color + offset + ' ' + sep + Style.RESET_ALL)
+            if b_file:
+                file_log.warning(offset + ' ' + sep)
+        #Play sound if requested
+        if b_sound:
+            winsound.Beep(1000,1000)
+    def critical(self, msg="", color="", level=0, file_logger_name='default_file_logger',
+             b_separator=False, b_sound=False, b_file=True):
+        """
+        Prints log message to console
+
+        Params
+        ==============
+        @msg : string
+            Message to be shown
+        @level : int
+            Indent level
+        @b_separator : Boolean
+            Show visual separator after the message?
+        @color : Fore.color
+            Color of the text
+        @b_sound : bool
+            Play sound after the message?
+        """
+        #Prepare loggers
+        #Fancy console
+        fancy_log = logging.getLogger('fancy_console_logger')
+        #File
+        file_log = logging.getLogger(file_logger_name)
+        #Default offset
+        offset = ''
+        #Print offset depending on the level
+        for i in range(0, level):
+            offset += '>>'
+        #Print message if there is one
+        if msg != "":
+            fancy_log.critical(color  + offset + ' ' + msg + Style.RESET_ALL)
+            if b_file:
+                file_log.critical(offset + ' ' + msg)
+        #Print separator if requested
+        if b_separator:
+            sep = ''
+            #Make the separator nicely aligned
+            for i in range(0, 25 - level):
+                sep += '=='
+            fancy_log.critical(color + offset + ' ' + sep + Style.RESET_ALL)
+            if b_file:
+                file_log.critical(offset + ' ' + sep)
+        #Play sound if requested
+        if b_sound:
+            winsound.Beep(1000,1000)
---
+++ run.py
@@ -0,0 +1,185 @@
+﻿#import requests
+#import json
+
+
+#from Miner import Archiver
+
+#import threading
+
+
+#url ='https://api.stocktwits.com/api/2/streams/symbol/AAPL.json'
+#data = {'since' : '51840090'}
+#data = {'limit' : '30'}
+#resp = requests.get(url, data)
+#out = json.loads(resp.content)
+
+#stream = StockTwitsStream(json = out['messages'])
+#print('test')
+
+
+#import matplotlib.pyplot as plt
+#import time
+#import random
+#from collections import deque
+#import numpy as np
+
+
+#a1 = deque([0]*100)
+#ax = plt.axes(xlim=(0, 20), ylim=(0, 40))
+
+#line, = plt.plot(a1)
+#plt.ion()
+#plt.ylim([0,40])
+#plt.show()
+
+#stream = StockTwitsStream()
+#url ='https://api.stocktwits.com/api/2/streams/symbol/AAPL.json'
+#resp = requests.get(url)
+#out = json.loads(resp.content)
+#stream.add_tweets(out['messages'])
+#xml = stream.dum_to_xml()
+#Archiver.save_xml_file(xml, './data/ST/', 'test.xml')
+
+#stream =  StockTwitsStream(path = './data/ST/test.xml')
+#stream.update('AAPL')
+#Archiver.save_xml_file(stream.dum_to_xml(), './data/ST/', 'test.xml')
+#for i in range(0,20):
+    
+#    resp = requests.get(url)
+#    out = json.loads(resp.content)
+#    a1.appendleft(stream.add_tweets(out))
+#    datatoplot = a1.pop()
+#    line.set_ydata(a1)
+#    plt.draw()
+#    print a1[0]
+#    i += 1
+#    time.sleep(60)
+#    plt.pause(0.0001)   
+
+#from Miner import MarketWatch
+
+#MM = MarketWatch('AAPL', True, 8000, './data/',0,0)
+#MM.start()
+#time.sleep(20)
+#print('still here')
+
+
+#MarketWatch.monitor_spread('AAPL', True, 1, 1, './data/')
+
+#import requests
+#import time
+#while True:
+#    quotes = requests.get("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG&e=.csv&f=nt1l1f6k3s7p2").text
+#    print(quotes)
+#    time.sleep(1)
+
+#from Miner import TorControl
+
+#def worker(num):
+#    """thread worker function"""
+#    tor = TorControl(num, num+1)
+#    tor.start_tor()
+#    tor.enable_proxy()
+#    while True:
+#        print 'Worker: %s' % num
+#        tor.new_identity(True)
+#    return
+
+#threads = []
+#for i in range(1,3):
+#    t = threading.Thread(target=worker, args=(i*3000,))
+#    threads.append(t)
+#    t.start()
+
+
+
+
+
+#import requests
+#import requesocks
+
+#from Miner import TorControl
+#tor = TorControl(9150, 9151)
+#tor.start_tor()
+#session = requesocks.session()
+
+#session.proxies = {
+#    'http': 'socks5://127.0.0.1:9150',
+#    'https': 'socks5://127.0.0.1:9150'
+#}
+
+#resp = requests.get('https://api.ipify.org?format=json', timeout=5)
+#j = json.loads(resp.content)
+#print('New IP: '+ j['ip'])
+
+#resp = session.get('https://api.ipify.org?format=json', timeout=5)
+#j = json.loads(resp.content)
+#print('New IP: '+ j['ip'])
+
+#Factor test
+#-----------
+#from factors import shortratio
+#from network import torcontrol
+from Factors import ShortRatio
+from Network import TorControl
+#with TorControl() as tor:
+#    short = ShortRatio('AAPL', proxies = tor.get_proxies())
+#print(short.value)
+
+#Screening test
+#---------------
+#from DataSources import GoogleFinance
+#print GoogleFinance.screen_stocks(0, 5)
+
+#Short squeeze strategy test
+#---------------------------
+from ShortSqueeze import ShortSqueeze
+#from Network import TorControl
+#from DataSources import YahooFinance as yf
+#with TorControl() as tor:
+#    ShortSqueeze.get_initial_universe(tor.get_proxies())
+#print 'done'
+
+#ShortSqueeze.update_universe_prices()
+#ShortSqueeze.update_universe_short_ratios()
+from Utility import Logger
+from colorama import Fore
+#u.log("test", 0, True, Fore.GREEN, True)
+#from Network import TorControl
+#with TorControl() as tor:
+#    for i in range(5):
+#        tor.new_identity()
+#        print(tor.get_current_ip())
+
+
+
+
+
+#logger = logging.getLogger(__name__)
+#logger.info('INFO')
+#logger.error('ERROR')
+#logger.debug('DEBUG')
+#u.test()
+#logger = Logger()
+#logger.critical("Test", level=3, color=Fore.LIGHTRED_EX, b_separator=True)
+
+#XML validation test
+from Xml import XmlValidator, XmlTransform
+xsd = './XML/Universe_sample.xsd'
+xml = './XML/Universe_sample.xml'
+xslt = './XML/Transform.xslt'
+
+#validator = XmlValidator(xsd)
+#test = validator.validate(xml)
+#print(test)
+#transform = XmlTransform(xslt)
+#result = transform.transform(xml)
+#print(result)
+
+#Sreener test
+from DataSources import Universe
+test_screener = Universe(xml,xslt,xsd)
+#with TorControl() as tor:
+#    test_screener.screener.handler.screen_one_page(proxies=tor.get_proxies(), start_at=10)
+#    pass
+test_screener.screen_all(True, 20)
